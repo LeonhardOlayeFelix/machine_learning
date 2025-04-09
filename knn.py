0f5303b52affc178132946e07f35aa850b6c4112
Leonhard <leonhard.olaye-felix@student.manchester.ac.uk>
2025-04-09 22:39:58 +0100

    initial commit

---
+++ knn.py
@@ -0,0 +1,115 @@
+from operator import index
+
+import numpy as np
+import matplotlib.pyplot as plt
+import scipy.sparse
+import scipy.stats
+
+data, labels, class_names, vocabulary = np.load("ReutersNews_4Classes_sparse.npy", allow_pickle=True)
+
+def sample_indices(labels, *num_per_class):
+    """
+    Returns randomly selected indices. It will return the specified number of indices for each class.
+    """
+
+
+    indices = []
+    for cls, num in enumerate(num_per_class):
+        cls_indices = np.where(labels == cls)[0]
+        indices.extend(np.random.choice(cls_indices, size=num, replace=False))
+    return np.array(indices)
+
+def get_pairwise_euclidian_distance(test_samples, training_data):
+    a = np.sum(test_samples.power(2), axis=1).reshape(-1, 1)
+    b = np.sum(training_data.power(2), axis=1).reshape(1, -1)
+    xfer = 2 * test_samples @ training_data.T
+    return np.sqrt(a + b - xfer)
+
+def get_pairwise_cosine_distance(test_samples, training_data):
+    a = np.sqrt(np.sum(test_samples.power(2), axis=1).reshape(test_samples.shape[0], 1))
+    b = np.sqrt(np.sum(training_data.power(2), axis=1).reshape(1, training_data.shape[0]))
+    return np.ones([test_samples.shape[0], training_data.shape[0]]) - (test_samples @ training_data.T) / (a * b)
+
+
+def knn_classify(test_samples, training_data, training_labels, metric="euclidean", k=1):
+    """
+    Performs k-nearest neighbour classification on the provided samples,
+    given training data and the corresponding labels.
+
+    test_samples: An m x d matrix of m samples to classify, each with d features.
+    training_data: An n x d matrix consisting of n training samples, each with d features.
+    training_labels: A vector of size n, where training_labels[i] is the label of training_data[i].
+    metric: The metric to use for calculating distances between samples.
+    k: The number of nearest neighbours to use for classification.
+
+    Returns: A vector of size m, where out[i] is the predicted class of test_samples[i].
+    """
+    # Calculate an m x n distance matrix.
+    pairwise_dist = np.zeros((test_samples.shape[0], training_labels.shape[0]))
+    if metric == "euclidean":
+        pairwise_dist = get_pairwise_euclidian_distance(test_samples, training_data)
+    else:
+        pairwise_dist = get_pairwise_cosine_distance(test_samples, training_data)
+    # Find the k nearest neighbours of each samples as an m x k matrix of indices.
+
+    nearest_neighbours = np.argsort(pairwise_dist, axis=1)[:, :k]
+
+    # Look up the classes corresponding to each index.
+    nearest_labels = training_labels[nearest_neighbours]
+    # Return the most frequent class on each row.
+    # Note: Ensure that the returned vector does not contain any empty dimensions.
+    # You may find the squeeze method useful here.
+    return np.squeeze(scipy.stats.mode(nearest_labels, axis=1)[0])
+
+def get_all_data():
+    train_indices = sample_indices(labels, 100,100,100,100)
+    test_indices = np.setdiff1d(np.arange(800), train_indices)
+    train_data = data[train_indices]
+    test_data = data[test_indices]
+    train_labels = labels[train_indices]
+    test_labels = labels[test_indices]
+    return train_data, test_data, train_labels, test_labels
+
+def knn_trial(num_trials, k, num_per_class_train, metric):
+    train_accuracies = np.zeros(num_trials)
+    test_accuracies = np.zeros(num_trials)
+    for i in range(20):
+        train_data, test_data, train_labels, test_labels = get_all_data()
+        train_prediction = knn_classify(train_data, train_data, train_labels, metric=metric, k=k)
+        test_prediction = knn_classify(test_data, train_data, train_labels, metric=metric, k=k)
+        train_accuracies[i] = np.sum(train_prediction == train_labels) / train_labels.size
+        test_accuracies[i] = np.sum(test_prediction == test_labels) / test_labels.size
+    return train_accuracies, test_accuracies
+
+
+def create_confusion_matrix(true_labels, pred_labels, classes=None):
+    if classes is None:
+        classes = np.union1d(np.unique(true_labels), np.unique(pred_labels))
+
+    n_classes = len(classes)
+    #Map labels to indices (0 to n_classes-1)
+    label_to_idx = {cls: idx for idx, cls in enumerate(classes)}
+
+    #Convert labels to indices
+    true_idx = np.array([label_to_idx[cls] for cls in true_labels])
+    pred_idx = np.array([label_to_idx[cls] for cls in pred_labels])
+
+    #Initialize confusion matrix
+    cm = np.zeros((n_classes, n_classes), dtype=int)
+
+    #Use np.add.at to accumulate counts
+    np.add.at(cm, (true_idx, pred_idx), 1)
+
+    return cm
+
+def main():
+
+    train_data, test_data, train_labels, test_labels = get_all_data()
+
+    predicted_labels = knn_classify(test_data, train_data, train_labels, metric="euclidean")
+
+    print(create_confusion_matrix(test_labels, predicted_labels))
+
+
+
+#print(knn_classify(A, B, np.random.randint(low=1, high=4, size=B.shape[0]), k=2))
---
+++ lab3.py
@@ -0,0 +1,130 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import scipy.sparse
+import scipy.stats
+from lab3lib import load_data, show_single_face, show_faces, partition_data, split_left_right, join_left_right, show_split_faces
+from knn import create_confusion_matrix
+def normalise_face_data(data):
+    return data/255
+
+def split_face_data(data, labels, num_per_class=3):
+    tr_ind, te_ind = partition_data(labels, num_per_class)
+    tr_data = data[tr_ind]
+    tr_label = labels[tr_ind]
+    te_data = data[te_ind]
+    te_label = labels[te_ind]
+    return tr_data, tr_label, te_data, te_label
+
+
+def l2_rls_train(train_data, train_labels, lmbd=0, multi=False):  # Add any other arguments here
+    """
+    A summary of your function goes here.
+
+    data: type and description of "data"
+    labels: type and description of "labels"
+    lmbd: type and description of "lmbd"
+
+    Returns: type and description of the returned variable(s).
+    """
+
+    # This is just to be consistent with the lecture notes.
+
+    if multi:
+        y = np.zeros((train_labels.shape[0], train_labels.max()))
+        rows = np.arange(train_labels.shape[0])
+        cols = train_labels-1
+        y[rows, cols] =  1
+        pass
+    else:
+        y = train_labels
+    X = train_data
+    # Expand X with a column of ones.
+    ones = np.ones(X.shape[0])
+    X_tilde = np.insert(X, 0, ones, axis=1)
+
+    # Compute the coefficient vector.
+
+    if lmbd == 0:
+        pseudo_inverse = np.linalg.pinv(X_tilde)
+    else:
+        pseudo_inverse = np.linalg.inv(X_tilde.T@X_tilde + lmbd * np.eye(X_tilde.shape[1]))@X_tilde.T
+
+    w = pseudo_inverse @ y
+
+    return w
+
+
+def l2_rls_predict(w, data):
+    """
+    A summary of your function goes here.
+
+    data: type and description of "data"
+    w: type and description of "w"
+
+    Returns: type and description of the returned variable(s).
+    """
+
+    # Compute the prediction.
+    ones = np.ones(data.shape[0])
+    x_tilde = np.insert(data, 0, ones, axis=1)
+    predicted_y = x_tilde @ w
+
+    return predicted_y
+
+def l2_rls_predict_class(predicted_y):
+    return np.argmax(predicted_y, axis=1) + 1
+
+def select_hyperparameter(tr_data, tr_label, lambdas, num_trials_per_lambda, verbose=False):
+    accs = np.zeros((len(lambdas), num_trials_per_lambda))
+
+    #Uses random subsampling to determine a choice of lambda
+    for i, lmbd in enumerate(lambdas):
+        for j in range(num_trials_per_lambda):
+            #Make an 80-20 split for training and validation data
+            train_data, train_label, valid_data, valid_label = split_face_data(tr_data, tr_label, 4)
+
+            #Train model on train data
+            w = l2_rls_train(train_data, train_label, lmbd=lmbd, multi=True)
+
+            #Determine the predicted class by taking the maximum certainty over the rows
+            #(one hot encoded)
+            predicted_y = l2_rls_predict(w, valid_data)
+            predicted_classes = l2_rls_predict_class(predicted_y)
+
+            #Determine the error rate between the true and predicted labels.
+            #Update error matrix
+            error_rate = np.mean(predicted_classes != valid_label)
+            accs[i, j] = error_rate
+
+    #Average error matrix over rows
+    avg_accs = np.mean(accs, axis=1)
+    if verbose:
+        print(avg_accs)
+    return lambdas[np.argmin(avg_accs)]
+
+def experiment_4(data, labels):
+    tr_data, tr_label, te_data, te_label = split_face_data(data, labels, 5)
+
+    #Hyperparameter selection
+    lambda_candidates = np.logspace(-1, 1, num=6, endpoint=False)
+    lmbd = select_hyperparameter(tr_data, tr_label, lambda_candidates, 3)
+
+    #Training
+    w = l2_rls_train(tr_data, tr_label, lmbd=lmbd, multi=True)
+
+    #Prediction
+    predicted_y = l2_rls_predict(w, te_data)
+    predicted_class = l2_rls_predict_class(predicted_y)
+    confusion_matrix = create_confusion_matrix(predicted_class, te_label)
+    return confusion_matrix
+
+def main():
+    data, labels = load_data()
+
+    data = normalise_face_data(data)
+    # tr_data, tr_label, te_data, te_label = split_face_data(data, labels)
+    # print(l2_rls_train(tr_data, tr_label).shape)
+    print(experiment_4(data, labels))
+
+
+main()
